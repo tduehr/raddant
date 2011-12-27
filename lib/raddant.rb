e4918772dbbca2b82948785e34d40617ad1b8887
tduehr <td@matasano.com>
2011-12-27 07:10:26 -0600

    should work

---
+++ lib/raddant.rb
@@ -0,0 +1,83 @@
+require 'tempfile'
+require 'digest/sha2'
+
+class Raddant
+  include Enumerable
+  class << self
+    attr_accessor :bin
+  end
+
+
+  self.bin = 'radamsa'
+
+  attr_accessor :indat
+  attr_reader :count, :seed, :fuzzers, :generators, :seek, :polymerase, :muxers, :muxer_polymerase
+  attr_reader :temp_file
+
+  def self.fuzz(file, opts={}, &block)
+    self.new(file, opts, block)
+  end
+
+  def initialize(file, opts={}, &block)
+    @indat = File.exists?(file) ? File.read(file) : file
+    @count = opts[:count]
+    @start_seed = opts[:seed]
+    @seed = @start_seed || Digest::SHA512.hexdigest(File.read("/dev/urandom",512))
+    @temp_file = Tempfile.new(opts[:temp_file] || "raddant")
+    @temp_file.close
+    @seek = opts[:seek]
+    @fuzzers = opts[:fuzzers]
+    @generators = opts[:generators]
+    @polymerase = opts[:polymerase]
+    @muxers = opts[:muxers]
+    @muxer_polymerase = opts[:muxer_polymerase]
+    self.each(block) if block_given?
+  end
+
+  def next?
+    (@count && @count < 1) ? false : true
+  end
+
+  def each
+    cnt = @count
+    sd = @seed
+    @seed = @start_seed || @seed
+    ret = []
+    while( self.next?) do
+      yield self.next
+      ret << @seed if @count
+    end
+    @count = cnt
+    @seed = sd
+    ret
+  end
+  
+  def next
+    execute
+    ret = ""
+    begin
+      @temp_file.open
+      ret = @temp_file.read
+    ensure
+      @temp_file.close
+    end
+    @seed = Digest::SHA512.hexdigest(@seed)
+    ret
+  end
+
+  private
+  def execute
+    args = ["-s", @seed, "-o", @temp_file.path]
+    @temp_file.open
+    @temp_file.write @indat
+    @temp_file.close
+    args += ["-f", [@fuzzers].flatten.join(",")] if @fuzzers
+    args += ["-g", [@generators].flatten.join(",")] if @generators
+    args += ["-p", [@polymerase].flatten.join(",")] if @polymerase
+    args += ["-m", [@muxers].flatten.join(",")] if @muxers
+    args += ["-P", [@muxer_polymerase].flatten.join(",")] if @muxer_polymerase
+    args += ["-S", @seek] if @seek
+
+    system(self.class.bin, *args)
+  end
+end
